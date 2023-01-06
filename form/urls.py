8edd8aa1cee6a18e0c94a93c7774c791b8d67149
maheh <maheshmb7712@gmail.com>
2023-01-06 10:13:54 +0530

    final

--- form/urls.py
+++ form/urls.py
@@ -4,4 +4,5 @@ from form import views
 
 urlpatterns = [
     path('form/', views.form, name='form'),
+    path('formview/', views.form_view, name='form_view'),
 ]
--- form/views.py
+++ form/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from .models import Form
 
 def form(request):
-
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
@@ -12,12 +11,12 @@ def form(request):
         queries = request.POST.get('query')
         forms=Form(name=name,email=email,phone_number=Phone_number,age=age,queries=queries)
         forms.save()
+        return render(request,'form/form2.html')
+    return render(request,'form/form.html')
+    
+def form_view(request):
         data=Form.objects.all()
         context={
              'data':data
-        }
+         }
         return render(request,'form/form1.html',context)
-    return render(request,'form/form.html')
-    
-
-        
